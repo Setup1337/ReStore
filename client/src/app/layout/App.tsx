f834b7a435ea8d68a55139d010a8d30711bace51
Setup1337 <thevadimchanel@gmail.com>
2022-05-26 18:05:07 +0200

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,11 @@
 import {Container, createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import ContactPage from '../../features/contact/ContactPage';
+import HomePage from '../../features/home/HomePage';
 
 import Header from './Header';
 
@@ -26,7 +31,11 @@ function App() {
       <CssBaseline/>  
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-          <Catalog />
+          <Route exact path ='/' component={HomePage}></Route>
+          <Route path ='/about' component={AboutPage}></Route>
+          <Route path ='/contact' component={ContactPage}></Route>
+          <Route exact path ='/catalog' component={Catalog}></Route>
+          <Route path ='/catalog/:id' component={ProductDetails}></Route>
       </Container>
     </ThemeProvider>
   );
